c5a8a15f0bee6dc3d20b83e3ff2ae9eb7da2cf91
Stepan-Piliasinskii <stepan.piliasinskii@gmail.com>
2026-04-03 02:30:34 +0200

    first one

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from app.api.routers import tasks
 from app.database import Base, engine
 
@@ -10,3 +11,6 @@ app = FastAPI(title="Task App")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(tasks.router, prefix="/tasks", tags=["tasks"])
+@app.get("/")
+def root():
+    return FileResponse("static/index.html")
